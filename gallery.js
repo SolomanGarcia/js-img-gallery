7577c935e303a15040ff1c9126832a46b32909ba
Soloman Garcia <soloman.garcia@gmail.com>
2020-05-21 08:50:17 -0600

    make next button function

--- gallery.js
+++ gallery.js
@@ -1,6 +1,13 @@
 const prevBtn = document.querySelector(".prev");
 const nextBtn = document.querySelector(".next");
+const galleryImgs = document.querySelectorAll(".gallery-img");
+let currentlySelected = 0;
 
 prevBtn.addEventListener("click", function () {});
 
-nextBtn.addEventListener("click", function () {});
+nextBtn.addEventListener("click", function () {
+  galleryImgs[currentlySelected].classList.remove("active");
+  currentlySelected++;
+  galleryImgs[currentlySelected].classList.add("active");
+  prevBtn.disabled = false;
+});
