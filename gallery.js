d6b811d43250bff1180f1f9ebe945eae09f5baba
Soloman Garcia <soloman.garcia@gmail.com>
2020-05-21 08:43:49 -0600

    work on js

---
+++ gallery.js
@@ -0,0 +1,6 @@
+const prevBtn = document.querySelector(".prev");
+const nextBtn = document.querySelector(".next");
+
+prevBtn.addEventListener("click", function () {});
+
+nextBtn.addEventListener("click", function () {});
